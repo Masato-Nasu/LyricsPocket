d1146178eee1619eb7e17f45febf70a240270b31
那須　雅人 <1bitexist@gmail.com>
2026-02-07 12:26:51 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -726,7 +726,7 @@ btnOnlineTranslate.addEventListener("click", () => {
 // ---------- Service Worker ----------
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js").catch(()=>{});
+    navigator.serviceWorker.register(new URL("sw.js", location.href)).catch(()=>{});
   });
 }
 
--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
 // LyricsPocket PWA service worker (app shell only)
-const CACHE = "lyricspocket-pwa-smartjp7-v1";
+const CACHE = "lyricspocket-pwa-smartjp8-v1";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(async (c) => {
+      const results = await Promise.allSettled(ASSETS.map(a => c.add(a)));
+      // ignore failures (e.g., when hosted under subpath); fetch handler will fall back
+      return results;
+    }).then(() => self.skipWaiting())
   );
 });
 
